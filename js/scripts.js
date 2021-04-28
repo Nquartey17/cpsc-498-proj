b9cc64d9d7d12a9b89b661f807fc311442019d15
Nquartey17 <niikwartei.quartey.17@cnu.edu>
2021-04-28 03:51:42 +0000

    Removed unnecessary files

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function calculate() {
-	var cgrade = parseFloat(document.getElementById("yourgrade").value);
-	var dgrade = parseFloat(document.getElementById("desgrade").value);
-	var fgrade = parseFloat(document.getElementById("finalgrade").value);
-	var total = (dgrade - cgrade * (100 - fgrade)) / fgrade;
-
-	var display = document.getElementById("submitbuttom").innerHTML;
-	document.getElementById("demo").innerHTML = display;
-}
